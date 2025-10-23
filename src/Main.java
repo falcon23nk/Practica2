40293d880c920a2e48cfa556560f644b41c0e0c9
Noussayr <nkorrja713@g.educaand.es>
2025-10-23 12:08:28 +0200

    Segundo commit: traducción al castellano

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola y bienvenidos!");
-
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
